25928ae66221adf4a2cee2e7d3ba942f4e117678
Aindra Moh <162561490+ainthrmo@users.noreply.github.com>
2026-09-06 19:45:38 +0630

    Fix MOSSQR favicon configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -153,9 +153,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-            <link rel="icon" href="/favicon.ico" />
-          <link rel="icon" type="image/png" sizes="512x512" href="/icon.png" />
-          <link rel="apple-touch-icon" href="/apple-icon.png" />
+        <link rel="icon" type="image/png" href="/apple-icon.png" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
